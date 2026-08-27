0500d9894e69d43d1af7f3d616b81dbcbe48763a
lspain <lspain@atu.edu>
2026-08-27 14:10:06 -0500

    all setters done

--- personTypelmp.cpp
+++ personTypelmp.cpp
@@ -16,3 +16,24 @@ void personType::print() const
 	cout << firstName << " " << middleName << " " << lastName << endl;
 }
 
+void personType::setName(string first, string middle, string last)
+{
+	firstName = first;
+	middleName = middle;
+	lastName = last;
+}
+
+void personType::setFirstName(string first)
+{
+	firstName = first;
+}
+
+void personType::setMiddleName(string middle)
+{
+	middleName = middle;
+}
+
+void personType::setLastName(string last)
+{
+	lastName = last;
+}
