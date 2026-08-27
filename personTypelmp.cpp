5c825bbf2f8b059f7fa4116d7194bfe671ca6633
lspain <lspain@atu.edu>
2026-08-27 14:13:15 -0500

    boolean functions

--- personTypelmp.cpp
+++ personTypelmp.cpp
@@ -11,11 +11,13 @@ personType::personType(string first, string middle, string last)
 	lastName = last;
 }
 
+// printing out all names
 void personType::print() const
 {
 	cout << firstName << " " << middleName << " " << lastName << endl;
 }
 
+// setters for all names
 void personType::setName(string first, string middle, string last)
 {
 	firstName = first;
@@ -23,6 +25,7 @@ void personType::setName(string first, string middle, string last)
 	lastName = last;
 }
 
+// setters for individual names
 void personType::setFirstName(string first)
 {
 	firstName = first;
@@ -36,4 +39,16 @@ void personType::setMiddleName(string middle)
 void personType::setLastName(string last)
 {
 	lastName = last;
+}
+
+
+bool personType::isFirstName(string first) const
+{
+	return (firstName == first);
+}
+
+
+bool personType::isLastName(string last) const
+{
+	return (lastName == last);
 }
