284dd0565c049082c867bf41ebb16c450b7c8193
lspain07 <lukasrspain@gmail.com>
2026-08-27 18:38:24 -0500

    adding the text and logic for name matching

--- Main.cpp
+++ Main.cpp
@@ -17,9 +17,30 @@ int main()
     cin >> middleName;
     cout << "Enter Your Last Name: ";
     cin >> lastName;
-    person.isFirstName(firstName);
-    person.isMiddleName(middleName);
-    person.isLastName(lastName);
+    if (person.isFirstName(firstName))
+    {
+        cout << "The first name matches." << endl;
+    }
+    else
+    {
+        cout << "The first name doesn't match." << endl;
+    }
+    if (person.isMiddleName(middleName))
+    {
+        cout << "The middle name matches." << endl;
+    }
+    else
+    {
+        cout << "The middle name doesn't match." << endl;
+    }
+    if (person.isLastName(lastName))
+    {
+        cout << "The last name matches." << endl;
+    }
+    else
+    {
+        cout << "The last name doesn't match." << endl;
+    }
     person.print(firstName, middleName, lastName);
 
 
