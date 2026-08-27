45e476db7eebff42d6a08cd9347ec4995fd40959
lspain <lspain@atu.edu>
2026-08-27 13:50:30 -0500

    copying code over from instructions pdf

---
+++ Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+
+using namespace std;
+
+int main() 
+{
+    // Write your main here 
+    return 0;
+}
--- personType.h
+++ personType.h
@@ -1,1 +1,33 @@
-#pragma once
+#ifndef PERSON_TYPE_H 
+#define PERSON_TYPE_H 
+
+#include <string> 
+
+using namespace std;
+
+class personType
+{
+public:
+
+    void print() const;
+    void setName(string first, string middle, string last);
+    void setLastName(string last);
+    void setFirstName(string first);
+    void setMiddleName(string middle);
+
+    bool isLastName(string last) const;
+    bool isFirstName(string first) const;
+
+    string getFirstName() const;
+    string getMiddleName() const;
+    string getLastName() const;
+
+    personType(string first = "", string middle = "", string last = "");
+
+private:
+    string firstName;
+    string middleName;
+    string lastName;
+};
+
+#endif
